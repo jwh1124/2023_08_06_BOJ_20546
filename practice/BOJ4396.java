fa99956b2165c7a003d60ffe1cc575b33d324d5a
mernyeok <jwh1124@naver.com>
2023-08-22 15:21:39 +0900

    12

--- practice/BOJ4396.java
+++ practice/BOJ4396.java
@@ -1,3 +1,5 @@
+package practice;
+
 import java.util.Scanner;
 import java.io.*;
 
@@ -9,49 +11,46 @@ public class BOJ4396 {
 
         String[] landmine_temp = new String[size];
         String[] open_temp = new String[size];
-        for(int i=0;i<size;i++){
+        for (int i = 0; i < size; i++) {
             landmine_temp[i] = scanner.nextLine();
         }
-        for(int i=0;i<size;i++){
+        for (int i = 0; i < size; i++) {
             open_temp[i] = scanner.nextLine();
         }
 
+        String[][] landmine = new String[size][size]; // 지뢰찾기 판
+        char[][] open = new char[size][size]; // 입력한 정답
 
-
-        String[][] landmine = new String[size][size]; //지뢰찾기 판
-        char[][] open = new char[size][size]; //입력한 정답 
-
-        for(int i=0; i<size; i++){
-            String temp = landmine_temp[i]
-            for(int j=0; j<size; j++){
-                landmine[i][j] = landmine_temp.split("");
-                System.out.println("현재 지뢰는?:"+landmine[i][j]+"i="+i+"j="+j);
+        for (int i = 0; i < size; i++) {
+            String temp = landmine_temp[i];
+            for (int j = 0; j < size; j++) {
+                System.out.println("현재 지뢰는?:" + landmine[i][j] + "i=" + i + "j=" + j);
             }
         }
         System.out.println("지뢰 입력");
-        // 스트링으로 받자니 문자 구분이 안돼서 split 쓰려그러면 split""는 사용하지 못한다고 하고.. 
+        // 스트링으로 받자니 문자 구분이 안돼서 split 쓰려그러면 split""는 사용하지 못한다고 하고..
         // char로 받자니 자바에는 따로 자바를 입력하는 함수가 없다그래서 charAt쓰려그러면 반복문을 어떻게 돌려야될지모르겠고...
 
-        for(int i=0; i<size; i++){
-            for(int j=0; j<size; j++){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 open[i][j] = scanner.next().charAt(j);
             }
         }
         System.out.println("정답 입력");
 
-        for(int i=0; i<size; i++){
-            for(int j=0; j<size; j++){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 System.out.print(landmine[i][j]);
             }
         }
 
         System.out.println("지뢰 출력");
 
-        for(int i=0; i<size; i++){
-            for(int j=0; j<size; j++){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 System.out.print(open[i][j]);
             }
-        }        
+        }
 
         System.out.println("정답 출력");
         scanner.close();
